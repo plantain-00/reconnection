a2ab6545db5e8b60478470f15bf4947e5b93773d
york yao <yaoao12306@outlook.com>
2017-10-13 13:24:56 +0800

    refactor lint scripts

--- demo/client.ts
+++ demo/client.ts
@@ -1,18 +1,17 @@
 import * as WebSocket from "ws";
 import Reconnector from "../dist/nodejs/nodejs";
 
+// tslint:disable:no-console
+
 let ws: WebSocket;
 const reconnector = new Reconnector(() => {
-    // tslint:disable-next-line:no-console
     console.log(`${new Date()} connecting...`);
     ws = new WebSocket("ws://localhost:8000");
     ws.onclose = () => {
-        // tslint:disable-next-line:no-console
         console.log(`${new Date()} disconnected...`);
         reconnector.reconnect();
     };
     ws.onopen = () => {
-        // tslint:disable-next-line:no-console
         console.log(`${new Date()} connected...`);
         reconnector.reset();
     };
