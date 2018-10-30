3e6da4c3bae72f74ec22047cde5b5b6e0e57e345
york yao <yaoao12306@outlook.com>
2018-10-30 23:01:01 +0800

    chore: add type-coverage check

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import resolve from 'rollup-plugin-node-resolve'
 export default {
   input: 'dist/browser/browser.js',
   name: 'Reconnector',
-  plugins: [resolve(), uglify()],
+  plugins: [resolve({ browser: true }), uglify()],
   output: {
     file: 'dist/reconnection.min.js',
     format: 'umd'
